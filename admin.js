f27bd00ec616cda4eb97c141eda9bd25ef94ebe7
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-30 17:28:05 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,45 @@
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT",
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+/* ================= MENU ================= */
+function addMenu(){
+  let text = document.getElementById("linkText").value;
+  let url = document.getElementById("linkUrl").value;
+
+  db.collection("menu").add({
+    text:text,
+    url:url
+  });
+
+  alert("Меню додано!");
+}
+
+/* ================= TRAINERS ================= */
+function addTrainer(){
+  let name = document.getElementById("trainerName").value;
+  let desc = document.getElementById("trainerDesc").value;
+
+  db.collection("trainers").add({
+    name:name,
+    desc:desc
+  });
+
+  alert("Тренера додано!");
+}
+
+/* ================= PHOTOS ================= */
+function addPhoto(){
+  let url = document.getElementById("photoUrl").value;
+
+  db.collection("photos").add({
+    url:url
+  });
+
+  alert("Фото додано!");
+}
