2a1d5d8e161dc788d215b0984eb1cb42229fbf6a
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-31 13:06:08 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
 const firebaseConfig = {
   apiKey: "YOUR_KEY",
   authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT"
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
