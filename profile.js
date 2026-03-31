0e3a46fe870206788e2fa84d316e7c567cba6b61
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-31 12:35:00 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,39 @@
 <!--логін користуача-->
+let user;
+
+auth.onAuthStateChanged(u=>{
+  if(!u) window.location="login.html";
+  user = u;
+  load();
+});
+
+function save(){
+  db.collection("users").doc(user.uid).set({
+    name: name.value
+  }, {merge:true});
+}
+
+function add(){
+  db.collection("trainings").add({
+    uid: user.uid,
+    date: date.value
+  });
+}
+
+function load(){
+  db.collection("trainings")
+  .where("uid","==",user.uid)
+  .onSnapshot(snap=>{
+    let html="";
+    snap.forEach(d=>{
+      html += `<p>${d.data().date}</p>`;
+    });
+    list.innerHTML = html;
+  });
+}
+
+function setTrainer(){
+  db.collection("users").doc(user.uid).update({
+    trainer: trainer.value
+  });
+}
