d777d5cfae9e2567ce4559462b17191f7242bc3b
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-31 12:39:54 +0300

    Update trainer.js

--- trainer.js
+++ trainer.js
@@ -1,1 +1,18 @@
 <!-- логін тренера-->
+db.collection("users").onSnapshot(snap=>{
+  let html="";
+
+  snap.forEach(u=>{
+    const data = u.data();
+
+    if(data.trainer){
+      html += `
+        <div class="card">
+          <p>${data.name || "Без імені"}</p>
+          <p>Твій клієнт</p>
+        </div>`;
+    }
+  });
+
+  clients.innerHTML = html;
+});
