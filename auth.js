be869cbd6d2dd8070719e1e599133b6ccf8f2b5c
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-31 16:18:37 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,11 +7,19 @@ auth.onAuthStateChanged(async user => {
     return;
   }
 
-  const doc = await db.collection("users").doc(user.uid).get();
-  const role = doc.data()?.role;
+  const ref = db.collection("users").doc(user.uid);
+  const doc = await ref.get();
+
+  if (!doc.exists) {
+    await ref.set({
+      role: "client",
+      name: "New user"
+    });
+  }
+
+  const role = (await ref.get()).data().role;
 
   if (role === "admin") window.location = "admin.html";
   if (role === "trainer") window.location = "trainer-profile.html";
   if (role === "client") window.location = "profile.html";
-
 });
