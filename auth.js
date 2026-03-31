0bd9bac2b07b0deea3276c23f26a37f6456ffd44
BabikovaN <a.kotcubenko.ua@icloud.com>
2026-03-31 12:31:40 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,17 @@
 <!--(логін)-->
+window.recaptchaVerifier =
+  new firebase.auth.RecaptchaVerifier('recaptcha');
+
+function login(){
+  const phone = document.getElementById("phone").value;
+
+  firebase.auth().signInWithPhoneNumber(phone, window.recaptchaVerifier)
+    .then(result=>{
+      const code = prompt("Код SMS:");
+      return result.confirm(code);
+    })
+    .then(()=>{
+      window.location = "profile.html";
+    })
+    .catch(e=>alert(e.message));
+}
